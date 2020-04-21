3ec290bc70d4e323fd33f5f5df9fa6447efb1170
Wendy Su <w696905@gmail.com>
2020-04-21 17:24:03 +0100

    data upgrade progress

--- amazon_comment_analysis.py
+++ amazon_comment_analysis.py
@@ -1,6 +1,9 @@
 comment = []
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		comment.append(line.strip())
-print(len(comment))
-print(comment[0])
+		count = count + 1
+		if count % 1000 == 0:
+			print(len(comment))
+print('There are', len(comment), ' comments in total.')
