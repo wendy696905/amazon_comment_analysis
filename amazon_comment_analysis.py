abf879ab00f97005e2dc712e9831e0bcb6d23750
Wendy Su <w696905@gmail.com>
2020-04-21 17:44:50 +0100

    average length of each comment

--- amazon_comment_analysis.py
+++ amazon_comment_analysis.py
@@ -7,3 +7,8 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(comment))
 print('There are', len(comment), ' comments in total.')
+
+sum_len = 0
+for d in comment:
+	sum_len = sum_len + len(d)
+print('The average quantity of the characters in the comment are', sum_len / len(comment))
